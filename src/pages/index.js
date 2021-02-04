7b018c5d387476a4b0699f0f328e8ce78830c4a3
Rejan Bajracharya <buzz.rejan@gmail.com>
2021-02-04 15:22:03 +0545

    styled-component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import React from "react"
 import Layout from '../components/Layout'
+import {ExampleButton} from '../components/button'
+
 export default function Home() {
   return <Layout>
    <h1>hello from gatsby</h1>
+
+<ExampleButton>click me</ExampleButton>
     </Layout>
 }
