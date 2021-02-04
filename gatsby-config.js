e271f3f64d24e01ec7ce6092804192cce7507c6c
Rejan Bajracharya <buzz.rejan@gmail.com>
2021-02-04 18:45:36 +0545

    gatsby-config source file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,20 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: 'gatsby tutorial',
-    description: 'some random description', 
-    author: 'john doe', 
-    data: ['item 1', 'item 2'], 
-    person: {name: 'peter', age: 32}
-  }, 
- plugins: [`gatsby-plugin-styled-components`], 
+    title: "gatsby tutorial",
+    description: "some random description",
+    author: "john doe",
+    data: ["item 1", "item 2"],
+    person: { name: "peter", age: 32 },
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
