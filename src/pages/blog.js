4ca7feafc1fcd6e606aa0fc66e75880ac4d15530
Rejan Bajracharya <buzz.rejan@gmail.com>
2021-02-04 14:11:01 +0545

    css modules

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import styles from '../components/blog.module.css'
 
 //global css problem : naming problem
-
 const blog = () => {
     return (
         <Layout>
+            <div className= {styles.page}>
+
             <h1>this is our blog page !</h1>
-            <p className= 'blog-text'>Minim cupidatat esse amet magna proident ea labore est incididunt fugiat in. Fugiat occaecat Lorem culpa ullamco eiusmod consectetur proident est officia et non cupidatat ex. Exercitation adipisicing consequat amet ea ipsum laborum eu.
+            <p className= {styles.text}>Minim cupidatat esse amet magna proident ea labore est incididunt fugiat in. Fugiat occaecat Lorem culpa ullamco eiusmod consectetur proident est officia et non cupidatat ex. Exercitation adipisicing consequat amet ea ipsum laborum eu.
             Sint veniam nulla sint in tempor duis laborum voluptate. 
             Magna duis pariatur reprehenderit sunt ut. 
             Nisi ex velit nulla exercitation eiusmod. 
             aliquip non nulla excepteur consectetur elit commodo
             aliqua occaecat esse labore est deserunt officia.
          </p>
+
+            </div>
         </Layout>
     )
 }
--- src/pages/product.js
+++ src/pages/product.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout'
+import styles from '../components/product.module.css'
 
 export default class product extends Component {
     render() {
         return (
             <Layout>
+                <div className= {styles.page} >
                 <h1>this is our product page!</h1>
                 <p className= 'product-text'>Minim cupidatat esse amet magna proident ea labore est incididunt fugiat in. Fugiat occaecat Lorem culpa ullamco eiusmod consectetur proident est officia et non cupidatat ex. Exercitation adipisicing consequat amet ea ipsum laborum eu.
             Sint veniam nulla sint in tempor duis laborum voluptate. 
@@ -13,6 +15,7 @@ export default class product extends Component {
             aliquip non nulla excepteur consectetur elit commodo
             aliqua occaecat esse labore est deserunt officia.
          </p>
+                </div>
             </Layout>
         )
     }
