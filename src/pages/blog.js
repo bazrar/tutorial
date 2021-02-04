168f14c49204d35dc5df808bedf8faa646c8a92f
Rejan Bajracharya <buzz.rejan@gmail.com>
2021-02-04 13:56:50 +0545

    global css

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,19 @@
 import React from 'react'
-
 import Layout from '../components/Layout'
+
+//global css problem : naming problem
 
 const blog = () => {
     return (
         <Layout>
             <h1>this is our blog page !</h1>
-            
+            <p className= 'blog-text'>Minim cupidatat esse amet magna proident ea labore est incididunt fugiat in. Fugiat occaecat Lorem culpa ullamco eiusmod consectetur proident est officia et non cupidatat ex. Exercitation adipisicing consequat amet ea ipsum laborum eu.
+            Sint veniam nulla sint in tempor duis laborum voluptate. 
+            Magna duis pariatur reprehenderit sunt ut. 
+            Nisi ex velit nulla exercitation eiusmod. 
+            aliquip non nulla excepteur consectetur elit commodo
+            aliqua occaecat esse labore est deserunt officia.
+         </p>
         </Layout>
     )
 }
--- src/pages/product.js
+++ src/pages/product.js
@@ -6,6 +6,13 @@ export default class product extends Component {
         return (
             <Layout>
                 <h1>this is our product page!</h1>
+                <p className= 'product-text'>Minim cupidatat esse amet magna proident ea labore est incididunt fugiat in. Fugiat occaecat Lorem culpa ullamco eiusmod consectetur proident est officia et non cupidatat ex. Exercitation adipisicing consequat amet ea ipsum laborum eu.
+            Sint veniam nulla sint in tempor duis laborum voluptate. 
+            Magna duis pariatur reprehenderit sunt ut. 
+            Nisi ex velit nulla exercitation eiusmod. 
+            aliquip non nulla excepteur consectetur elit commodo
+            aliqua occaecat esse labore est deserunt officia.
+         </p>
             </Layout>
         )
     }
