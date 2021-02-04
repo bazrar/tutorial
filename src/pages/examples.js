23551422a6f3fc3f4eb0bc7a58421df590978ad5
Rejan Bajracharya <buzz.rejan@gmail.com>
2021-02-04 17:26:09 +0545

    page graphql

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,15 +2,37 @@ import React from 'react'
 import Layout from '../components/Layout'
 import Header from '../components/Header'
 import HeaderStatic from '../examples/HeaderStatic'
+import {graphql} from 'gatsby'
 
-const examples = () => {
+
+const examples = ({data}) => {
+   const {site: {info: {title, person: {name, age}}}} = data;
     return (
         <Layout>
        <h1>Hello from examples page</h1>
        <Header />
        <HeaderStatic />
+       <h5>title: {title}</h5>
+       <h5>name: {name}</h5>
        </Layout>
     )
 }
 
+export const data = graphql`
+{
+  site {
+    info:siteMetadata {
+      author
+      data
+      description
+      title
+      person {
+        age
+        name
+      }
+    }
+  }
+}
+
+`
 export default examples
